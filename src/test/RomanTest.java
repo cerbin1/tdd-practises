b14d05dd988719505ff421f0486b6dd40346b8bd
cerbin <gekarez@gmail.com>
2023-05-13 19:10:06 +0200

    Allow two as roman numeral

--- src/test/RomanTest.java
+++ src/test/RomanTest.java
@@ -8,6 +8,11 @@ public class RomanTest {
         Assert.assertEquals("I", formatRoman(1));
     }
 
+    @Test
+    public void shouldFormatTwo() {
+        Assert.assertEquals("II", formatRoman(2));
+    }
+
     @Test(expected = IllegalArgumentException.class)
     public void shouldThrowForNegativeOne() {
         formatRoman(-1);
@@ -23,10 +28,19 @@ public class RomanTest {
         formatRoman(0);
     }
 
+
     private String formatRoman(int i) {
         if (i < 1) {
             throw new IllegalArgumentException();
         }
-        return "I";
+        return formatRomanNonNegative(i);
+    }
+
+    private static String formatRomanNonNegative(int i) {
+        String result = "I";
+        if (i == 2) {
+            result += "I";
+        }
+        return result;
     }
 }
